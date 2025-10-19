ed7edb4b9839ea1d3c4f3ee0f772b16adf20ebab
parkboseung <endeveloper96@gmail.com>
2025-10-20 00:39:55 +0900

    포인트 조회 하드코딩으로 1000L 포인트 넘겨주기

---
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -0,0 +1,8 @@
+package io.hhplus.tdd.point.service;
+
+public class PointService {
+
+    public long getPoint(long id) {
+        return 1000L;
+    }
+}
--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -2,11 +2,17 @@ package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.UserPoint;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 public class PointServiceTest {
 
     @Mock
@@ -17,10 +23,11 @@ public class PointServiceTest {
 
     @Test
     @DisplayName("포인트 조회 실패")
-    void selectUserPoint_fail() {
+    void 포인트_조회_실패() {
         // given 사용자 1번이 1000 포인트를 가지고 있다.
-        long id = 1;
+        long id = 1L;
         userPointTable.insertOrUpdate(id, 1000L);
-        int point = PointService.getPoint(id);
+        long point = pointService.getPoint(id);
+        assertThat(point).isEqualTo(1000L);
     }
 }
